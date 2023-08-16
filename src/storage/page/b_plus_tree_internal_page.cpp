8ca12e5c6e33496161774335f02b46beda7d9e35
gnolong <2391353625@qq.com>
2023-08-16 10:53:24 +0800

    project 1,submit,format

--- src/storage/page/b_plus_tree_internal_page.cpp
+++ src/storage/page/b_plus_tree_internal_page.cpp
@@ -25,9 +25,7 @@ namespace bustub {
  * Including set page type, set current size, and set max page size
  */
 INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(int max_size) {
-  BPlusTreePage::SetMaxSize(max_size);
-}
+void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(int max_size) { BPlusTreePage::SetMaxSize(max_size); }
 /*
  * Helper method to get/set the key associated with input "index"(a.k.a
  * array offset)
