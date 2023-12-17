1d603a384374b904e7d973005b5e82d9f5152897
long <2391353625@qq.com>
2023-12-17 13:50:54 +0800

    project 3,hash join 1.0

--- src/execution/hash_join_executor.cpp
+++ src/execution/hash_join_executor.cpp
@@ -11,21 +11,132 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/hash_join_executor.h"
+#include "type/value_factory.h"
 
 namespace bustub {
 
 HashJoinExecutor::HashJoinExecutor(ExecutorContext *exec_ctx, const HashJoinPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&left_child,
                                    std::unique_ptr<AbstractExecutor> &&right_child)
-    : AbstractExecutor(exec_ctx) {
-  if (!(plan->GetJoinType() == JoinType::LEFT || plan->GetJoinType() == JoinType::INNER)) {
+    : AbstractExecutor(exec_ctx), plan_(plan), left_child_(std::move(left_child)), 
+      right_child_(std::move(right_child)), lft_itr_(lft_ht_.End()), itr_(valuess_.end()){
+  if (plan->GetJoinType() != JoinType::LEFT && plan->GetJoinType() != JoinType::INNER) {
     // Note for 2023 Spring: You ONLY need to implement left join and inner join.
     throw bustub::NotImplementedException(fmt::format("join type {} not supported", plan->GetJoinType()));
   }
 }
 
-void HashJoinExecutor::Init() { throw NotImplementedException("HashJoinExecutor is not implemented"); }
+void HashJoinExecutor::Init() {
+  left_child_->Init();
+  itr_ = valuess_.begin();
+}
 
-auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool { return false; }
+auto HashJoinExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  std::cout << "throught hashjoin executor" << '\n';
+  if(itr_ != valuess_.end()){
+    *tuple = {*itr_, &GetOutputSchema()};
+    ++itr_;
+    *rid = RID{};
+    return true;
+  }
+  if(not_first_call_){
+    return false;
+  }
+  not_first_call_ = true;
+  Tuple left_tuple{};
+  Tuple right_tuple{};
+  RID rid_t{};
+  if(!left_child_->Next(&left_tuple, &rid_t)){
+    return false;
+  }
+    auto schema_lft = plan_->GetLeftPlan()->OutputSchema();
+    auto schema_rt = plan_->GetRightPlan()->OutputSchema();
+  if(plan_->GetJoinType() == JoinType::LEFT){
+    std::vector<Value> values_null;
+    bool null_done{false};
+    do{
+      std::vector<Value> values_left;
+      bool left_done{false};
+      auto len_lft = schema_lft.GetColumnCount();
+      auto len_rt = schema_rt.GetColumnCount();
+      bool joined{false};
+      right_child_->Init();
+      while(right_child_->Next(&right_tuple, &rid_t)){
+        auto val = plan_->Predicate()->EvaluateJoin(&left_tuple, schema_lft, 
+                                          &right_tuple, schema_rt);
+        if(!val.IsNull() && val.GetAs<bool>()){
+          joined = true;
+          std::vector<Value> values;
+          values.reserve(len_lft + len_rt);
+          if(!left_done){
+            for(uint32_t i = 0; i < len_lft; ++i){
+              values_left.push_back(left_tuple.GetValue(&schema_lft, i));
+            left_done = true;
+          }
+          }
+          values.insert(values.end(),values_left.begin(), values_left.end());
+          for(uint32_t i = 0; i < len_rt; ++i){
+            values.push_back(right_tuple.GetValue(&schema_rt, i));
+          }
+          valuess_.push_back(std::move(values));
+        }                                 
+      }
+      if(!joined){
+        if(!null_done){
+          for(uint32_t i = 0; i < len_rt; ++i){
+            values_null.push_back(ValueFactory::GetNullValueByType(schema_rt.GetColumn(i).GetType()));
+          }
+          null_done = true;
+        }
+        std::vector<Value> values;
+        values.reserve(len_lft + len_rt);
+        for(uint32_t i = 0; i < len_lft; ++i){
+          values.push_back(left_tuple.GetValue(&schema_lft, i));
+        }
+        values.insert(values.end(),values_null.begin(), values_null.end());
+        valuess_.push_back(std::move(values));
+      }
+    }while(left_child_->Next(&left_tuple, &rid_t));
+    right_child_->Init();
+  }
+  else if(plan_->GetJoinType() == JoinType::INNER){
+    do{
+      std::vector<Value> values_left;
+      bool left_done{false};
+      auto len_lft = schema_lft.GetColumnCount();
+      auto len_rt = schema_rt.GetColumnCount();
+      right_child_->Init();
+      while(right_child_->Next(&right_tuple, &rid_t)){
+        auto val = plan_->Predicate()->EvaluateJoin(&left_tuple, schema_lft, 
+                                          &right_tuple, schema_rt);
+        if(!val.IsNull() && val.GetAs<bool>()){
+          std::vector<Value> values;
+          values.reserve(len_lft + len_rt);
+          if(!left_done){
+            for(uint32_t i = 0; i < len_lft; ++i){
+              values_left.push_back(left_tuple.GetValue(&schema_lft, i));
+            left_done = true;
+          }
+          }
+          values.insert(values.end(),values_left.begin(), values_left.end());
+          for(uint32_t i = 0; i < len_rt; ++i){
+            values.push_back(right_tuple.GetValue(&schema_rt, i));
+          }
+          valuess_.push_back(std::move(values));
+        }                                 
+      }
+    }while(left_child_->Next(&left_tuple, &rid_t));
+    right_child_->Init();
+  }
+  itr_ = valuess_.begin();
+  if(itr_ == valuess_.end()){
+    return false;
+  }
+  *tuple = {*itr_, &GetOutputSchema()};
+  ++itr_;
+  *rid = RID{};
+  return true;
+
+}
 
 }  // namespace bustub
--- src/include/execution/executors/hash_join_executor.h
+++ src/include/execution/executors/hash_join_executor.h
@@ -12,15 +12,131 @@
 
 #pragma once
 
+#include <cassert>
+#include <cstdint>
 #include <memory>
+#include <type_traits>
 #include <utility>
 
+#include "common/exception.h"
+#include "common/util/hash_util.h"
 #include "execution/executor_context.h"
 #include "execution/executors/abstract_executor.h"
 #include "execution/plans/hash_join_plan.h"
 #include "storage/table/tuple.h"
 
 namespace bustub {
+
+struct HashjoinKey {
+  std::vector<Value> values_;
+
+  /**
+   * Compares two Join keys for equality.
+   * @param other the other Join key to be compared with
+   * @return `true` if both Join keys have equivalent group-by expressions, `false` otherwise
+   */
+  auto operator==(const HashjoinKey &other) const -> bool {
+    for (uint32_t i = 0; i < other.values_.size(); i++) {
+      if (values_[i].CompareEquals(other.values_[i]) != CmpBool::CmpTrue) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+/** HashjoinValue represents a value for each of the running Joins */
+struct HashjoinValue {
+  /** The Join values */
+  std::vector<Value> values_;
+};
+
+}
+namespace std {
+
+/** Implements std::hash on HashjoinKey */
+template <>
+struct hash<bustub::HashjoinKey> {
+  auto operator()(const bustub::HashjoinKey &key) const -> std::size_t {
+    size_t curr_hash = 0;
+    for (const auto &key : key.values_) {
+      if (!key.IsNull()) {
+        curr_hash = bustub::HashUtil::CombineHashes(curr_hash, bustub::HashUtil::HashValue(&key));
+      }
+    }
+    return curr_hash;
+  }
+};
+
+}  // namespace std
+namespace bustub {
+
+
+/**
+ * A simplified hash table that has all the necessary functionality for Joins.
+ */
+class SimpleJoinHashTable {
+ public:
+  SimpleJoinHashTable() = default;
+  /**
+   * Inserts a value into the hash table and then combines it with the current Join.
+   * @param agg_key the key to be inserted
+   * @param agg_val the value to be inserted
+   */
+  void Insert(const HashjoinKey &key, const HashjoinValue &val) {
+    if (ht_.count(key) == 0) {
+      
+    }
+  }
+
+  /**
+   * Clear the hash table
+   */
+  void Clear() { ht_.clear(); }
+
+  /** An iterator over the Join hash table */
+  class Iterator {
+   public:
+    /** Creates an iterator for the Join map. */
+    explicit Iterator(std::unordered_map<HashjoinKey, HashjoinValue>::const_iterator iter) : iter_{iter} {}
+
+    /** @return The key of the iterator */
+    auto Key() -> const HashjoinKey & { return iter_->first; }
+
+    /** @return The value of the iterator */
+    auto Val() -> const HashjoinValue & { return iter_->second; }
+
+    /** @return The iterator before it is incremented */
+    auto operator++() -> Iterator & {
+      ++iter_;
+      return *this;
+    }
+
+    /** @return `true` if both iterators are identical */
+    auto operator==(const Iterator &other) -> bool { return this->iter_ == other.iter_; }
+
+    /** @return `true` if both iterators are different */
+    auto operator!=(const Iterator &other) -> bool { return this->iter_ != other.iter_; }
+
+   private:
+    /** Joins map */
+    std::unordered_map<HashjoinKey, HashjoinValue>::const_iterator iter_;
+  };
+
+  /** @return Iterator to the start of the hash table */
+  auto Begin() -> Iterator { return Iterator{ht_.cbegin()}; }
+
+  /** @return Iterator to the end of the hash table */
+  auto End() -> Iterator { return Iterator{ht_.cend()}; }
+
+  auto operator[](const HashjoinKey& key) -> HashjoinValue&{
+    return ht_[key];
+  }
+ private:
+  /** The hash table is just a map from Join keys to Join values */
+  std::unordered_map<HashjoinKey, HashjoinValue> ht_{};
+};
+
 
 /**
  * HashJoinExecutor executes a nested-loop JOIN on two tables.
@@ -52,8 +168,63 @@ class HashJoinExecutor : public AbstractExecutor {
   auto GetOutputSchema() const -> const Schema & override { return plan_->OutputSchema(); };
 
  private:
+  /** @return The tuple as an JoinKey */
+  enum JType {left, right};
+
+  auto MakeJoinKey(const Tuple *tuple, const JType type) -> HashjoinKey {
+    std::vector<Value> keys;
+    if(type == JType::left){
+      for (const auto &expr : plan_->LeftJoinKeyExpressions()) {
+        keys.emplace_back(expr->Evaluate(tuple, left_child_->GetOutputSchema()));
+      }
+    }
+    else if(type == JType::right){
+      for (const auto &expr : plan_->RightJoinKeyExpressions()) {
+        keys.emplace_back(expr->Evaluate(tuple, right_child_->GetOutputSchema()));
+      }
+    }
+    return {keys};
+  }
+
+  /** @return The tuple as an JoinValue */
+  auto MakeJoinValue(const Tuple *tuple, const JType type) -> HashjoinValue {
+    std::vector<Value> vals;
+    if(type == JType::left){
+      auto schema = plan_->GetLeftPlan()->OutputSchema();
+      auto len = schema.GetColumnCount();
+      for(uint32_t i = 0; i < len; ++i){
+        vals.emplace_back(tuple->GetValue(&schema, i));
+      }
+    }
+    if(type == JType::right){
+      auto schema = plan_->GetRightPlan()->OutputSchema();
+      auto len = schema.GetColumnCount();
+      for(uint32_t i = 0; i < len; ++i){
+        vals.emplace_back(tuple->GetValue(&schema, i));
+      }
+    }
+    return {vals};
+  }
   /** The NestedLoopJoin plan node to be executed. */
   const HashJoinPlanNode *plan_;
+
+  std::unique_ptr<AbstractExecutor> left_child_;
+
+  std::unique_ptr<AbstractExecutor> right_child_;
+
+  SimpleJoinHashTable lft_ht_{};
+
+  SimpleJoinHashTable::Iterator lft_itr_;
+
+  // SimpleJoinHashTable rt_ht_;
+
+  // SimpleJoinHashTable::Iterator rt_itr_;
+
+  std::vector<std::vector<Value>> valuess_{};
+  
+  std::vector<std::vector<Value>>::iterator itr_;
+
+  bool not_first_call_{false};
 };
 
 }  // namespace bustub
